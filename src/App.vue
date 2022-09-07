7d98d14e94055b5cc367bdb2f08b90a21c8147be
Kristof Gilicze <giliczekristof@gmail.com>
2022-09-07 15:55:17 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,7 @@ const getPage = (): number => {
 let currentPage = $ref<number>(getPage())
 const zoom = ref<number>(85)
 
-const urlFull = './assets/LLBG_latvany_20220223_vegleges.pdf'
-const url = computed(() => `./assets/pages/LLBG_latvany_20220223_vegleges-${currentPage}.pdf`);
+const url = computed(() => `./assets/pages/PP_katalogus_202200815_2-${currentPage}.pdf`);
 
 function navLeft() {
   currentPage--;
@@ -45,56 +44,26 @@ watch(() => currentPage, () => {
 <template>
   <main class="flex flex-row min-h-screen w-full gap-5 justify-around">
     <button v-show="currentPage != 1" @click="navLeft" type="button">
-      <svg
-        class="h-6 w-6 dark:text-white"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M7 16l-4-4m0 0l4-4m-4 4h18"
-        />
+      <svg class="h-6 w-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
       </svg>
     </button>
     <div class="flex flex-col gap-5 w-full min-h-screen justify-center items-center">
-      <input
-        class="absolute top-0 m-3 rounded-lg overflow-hidden appearance-none bg-gray-400 h-6 w-128"
-        type="range"
-        min="0"
-        max="150"
-        step="10"
-        v-model="zoom"
-      />
+      <input class="absolute top-0 m-3 rounded-lg overflow-hidden appearance-none bg-gray-400 h-6 w-128" type="range"
+        min="0" max="150" step="10" v-model="zoom" />
       <PdfViewer :path="url" :zoom="zoom" />
       <nav class="absolute bottom-0 flex flex-row justify-center items-center h-24 w-256">
-        <input
-          v-model="currentPage"
-          type="number"
-          :max="MAX"
+        <input v-model="currentPage" type="number" :max="MAX"
           class="h-10 w-24 bg-gray-300 border border-gray-300 text-gray-900 text-lg text-center rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-        />
+          required />
         <OpenExternalButton :url="url" />
       </nav>
     </div>
     <button @click="navRight" type="button">
-      <svg
-        class="h-6 w-6 dark:text-white"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M17 8l4 4m0 0l-4 4m4-4H3"
-        />
+      <svg class="h-6 w-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
       </svg>
     </button>
   </main>
@@ -110,21 +79,6 @@ main {
 
 button,
 a {
-  @apply rounded-lg
-   font-medium
-    bg-dark-700
-     m-5
-      text-white
-       text-sm
-        text-center
-         p-2.5
-          inline-flex
-           items-center
-            dark:bg-blue-600
-             hover:bg-gray-400
-              focus:ring-4
-               focus:ring-blue-300
-                dark:hover:bg-blue-700
-                dark:focus:ring-blue-800;
+  @apply rounded-lg font-medium bg-dark-700 m-5 text-white text-sm text-center p-2.5 inline-flex items-center dark: bg-blue-600 hover:bg-gray-400 focus:ring-4 focus:ring-blue-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800;
 }
 </style>
