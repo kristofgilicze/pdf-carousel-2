8dae99a7ded8f86d015af51be1342643ee6f13b3
adamkrizsan <lovidmann@gmail.com>
2024-02-03 16:47:41 +0100

    Added experimental mobile support

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ const zoom = ref<number>(85)
 // /home/kristofg/pdf-carousel-2/src/assets/pages/42_PP2024.pdf
 const url = computed(() => `./assets/pages/${currentPage}_PP2024.pdf`);
 
+
 function navLeft() {
   currentPage--;
 }
@@ -42,30 +43,37 @@ watch(() => currentPage, () => {
 </script>
 
 <template>
-  <main class="flex flex-row min-h-screen w-full gap-5 justify-around">
-    <button v-show="currentPage != 1" @click="navLeft" type="button">
-      <svg class="h-6 w-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
-      </svg>
-    </button>
-    <div class="flex flex-col gap-5 w-full min-h-screen justify-center items-center">
+  
+  <main class="flex flex-col h-screen w-full justify-around">
+    
+    <div class="flex flex-col flex-grow gap-5 w-full justify-center items-center">
       <input class="absolute top-0 m-3 rounded-lg overflow-hidden appearance-none bg-gray-400 h-6 w-128" type="range"
         min="0" max="150" step="10" v-model="zoom" />
-      <PdfViewer :path="url" :zoom="zoom" />
-      <nav class="absolute bottom-0 flex flex-row justify-center items-center h-24 w-256">
-        <input v-model="currentPage" type="number" :max="MAX"
-          class="h-10 w-24 bg-gray-300 border border-gray-300 text-gray-900 text-lg text-center rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required />
-        <OpenExternalButton :url="url" />
-      </nav>
+        <PdfViewer :path="url" :zoom="zoom"  />
+      
     </div>
-    <button @click="navRight" type="button">
-      <svg class="h-6 w-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-      </svg>
-    </button>
+    <div class="flex items-stretch items-center justify-between">
+      <button v-show="currentPage != 1" @click="navLeft" type="button" class=" flex  h-10 w-10">
+        <svg class="h-6 w-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+        </svg>
+      </button>
+      <nav class="flex basis-1/2 items-center items-stretch">
+        <OpenExternalButton :url="url" class="flex flex-grow items-stretch basis-1/4" />
+        <input v-model="currentPage" type="number" :max="MAX"
+          class="self-center flex-grow basis-1/4 self-strecth h-10 w-24 flex bg-gray-300 border border-gray-300 text-gray-900 text-lg text-center rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required />
+        
+      </nav>
+      <button @click="navRight" type="button" class="h-10 w-10 ">
+        <svg class="h-6 w-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        </svg>
+      </button>
+    </div>
+
   </main>
 </template>
 
@@ -82,3 +90,5 @@ a {
   @apply rounded-lg font-medium bg-dark-700 m-5 text-white text-sm text-center p-2.5 inline-flex items-center dark: bg-blue-600 hover:bg-gray-400 focus:ring-4 focus:ring-blue-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800;
 }
 </style>
+
+
--- src/components/OpenExternalButton.vue
+++ src/components/OpenExternalButton.vue
@@ -11,14 +11,15 @@ const props = defineProps({
 
 <template>
     <a
-        class="rounded-lg font-medium bg-dark-700 m-5 text-white text-sm text-center p-2.5 inline-flex items-center dark:bg-blue-600 hover:bg-gray-400 focus:ring-4 focus:ring-blue-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="rounded-lg flex inline-flex  font-medium bg-dark-700 m-5 text-white text-sm text-center items-center dark:bg-blue-600 hover:bg-gray-400 focus:ring-4 focus:ring-blue-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         :href="url"
         target="_blank"
         title="Megnyitás PDF olvasóban"
     >
+    
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            class="h-6 w-full "
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -30,6 +31,5 @@ const props = defineProps({
                 d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
             />
         </svg>
-        lap letöltése
     </a>
 </template>
